c2bf26ea5bb49d6603cdad38dfcd4aac7ff2b9a4
Asadbek <dasturchi6678@gmail.com>
2023-07-27 16:36:07 +0500

    Initial Commit

---
+++ udemyiosnative/udemyiosnativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemyiosnativeApp.swift
+//  udemyiosnative
+//
+//  Created by Dark Laki on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct udemyiosnativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
