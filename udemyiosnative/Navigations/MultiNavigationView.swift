34c71ff6a3c8713070e091fcd7b259c15cb1fa3c
asadbek-ip <dasturchi6678@gmail.com>
2023-08-15 12:20:35 +0500

    searchbar

---
+++ udemyiosnative/Navigations/MultiNavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  MultiNavigationView.swift
+//  udemyiosnative
+//
+//  Created by Mac Mini 1 on 14/08/23.
+//
+
+import SwiftUI
+
+struct MultiNavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiNavigationView()
+}
